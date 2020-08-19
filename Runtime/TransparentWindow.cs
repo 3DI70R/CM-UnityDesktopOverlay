d926da46f95ccb741ac0e427da026636cdd060da
Maxim.Bagashev <ThreeDISevenZeroR@gmail.com>
2020-08-20 02:24:53 +0400

    Removal of unused logic

--- Runtime/TransparentWindow.cs
+++ Runtime/TransparentWindow.cs
@@ -1,172 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using Debug = UnityEngine.Debug;
 
-[RequireComponent(typeof(Camera))]
 public class TransparentWindow : MonoBehaviour
 {
-	public static TransparentWindow Main = null;
-	public static Camera Camera = null;	//Used instead of Camera.main
+	private List<Func<Vector2, bool>> isFocusedHandlers 
+		= new List<Func<Vector2, bool>>();
 
-	[Tooltip("What GameObject layers should trigger window focus when the mouse passes over objects?")] //
-	[SerializeField] LayerMask clickLayerMask = ~0;
+	private int screenWidth;
+	private int screenHeight;
+	private IntPtr hwnd = IntPtr.Zero;
+	private Rectangle margins;
+	private Rectangle windowRect;
 
-	[Tooltip("Allows Input to be detected even when focus is lost")] //
-	[SerializeField] bool useSystemInput = false;
-
-	[Tooltip("Should the window be fullscreen?")] //
-	[SerializeField] bool fullscreen = true;
-
-	[Tooltip("Force the window to match ScreenResolution")] //
-	[SerializeField] bool customResolution = true;
-
-	[Tooltip("Resolution the overlay should run at")] //
-	[SerializeField] Vector2Int screenResolution = new Vector2Int(1280, 720);
-
-	[Tooltip("The framerate the overlay should try to run at")] //
-	[SerializeField] int targetFrameRate = 30;
-
-	
-	/////////////////////
-	//Windows DLL stuff//
-	/////////////////////
-	
-	[DllImport("user32.dll")]
-	static extern IntPtr GetActiveWindow();
-	
-	[DllImport("user32.dll")]
-	static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-
-	[DllImport("user32.dll", EntryPoint = "SetLayeredWindowAttributes")]
-	static extern int SetLayeredWindowAttributes(IntPtr hwnd, int crKey, byte bAlpha, int dwFlags);
-
-	[DllImport("user32.dll", EntryPoint = "GetWindowRect")]
-	static extern bool GetWindowRect(IntPtr hwnd, out Rectangle rect);
-	
-	[DllImport("user32.dll")]
-	static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
-
-	[DllImportAttribute("user32.dll")]
-	static extern bool ReleaseCapture();
-
-	[DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-	static extern int SetWindowPos(IntPtr hwnd, int hwndInsertAfter, int x, int y, int cx, int cy, int uFlags);
-
-	[DllImport("Dwmapi.dll")]
-	static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Rectangle margins);
-
-	const int GWL_STYLE = -16;
-	const uint WS_POPUP = 0x80000000;
-	const uint WS_VISIBLE = 0x10000000;
-	const int HWND_TOPMOST = -1;
-
-	const int WM_SYSCOMMAND = 0x112;
-	const int WM_MOUSE_MOVE = 0xF012;
-
-	int fWidth;
-	int fHeight;
-	IntPtr hwnd = IntPtr.Zero;
-	Rectangle margins;
-	Rectangle windowRect;
-
-	//BUG: Sometimes fails to SetResolution if not focused on startup - if using Start(), WindowBoundsCollider2D sometimes fails to set the correct size
-	void Awake()
+	public void AddFocusHandler(Func<Vector2, bool> focusHandler)
 	{
-		Main = this;
+		isFocusedHandlers.Add(focusHandler);
+	}
 
-		Camera = GetComponent<Camera>();
-		Camera.backgroundColor = new Color();
-		Camera.clearFlags = CameraClearFlags.SolidColor;
+	public void RemoveFocusHandler(Func<Vector2, bool> focusHandler)
+	{
+		isFocusedHandlers.Remove(focusHandler);
+	}
 
-		if (fullscreen && !customResolution)
-		{
-			screenResolution = new Vector2Int(Screen.currentResolution.width, Screen.currentResolution.height);
-		}
-		
-		Screen.SetResolution(screenResolution.x, screenResolution.y, fullscreen ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed);
+	private void Awake()
+	{
+		screenWidth = Screen.currentResolution.width;
+		screenHeight = Screen.currentResolution.height;
 
-		Application.targetFrameRate = targetFrameRate;
+		Screen.SetResolution(screenWidth, screenHeight, FullScreenMode.FullScreenWindow);
 		Application.runInBackground = true;
 
-#if !UNITY_EDITOR
-		fWidth = screenResolution.x;
-		fHeight = screenResolution.y;
-		margins = new Rectangle() {Left = -1};
-		hwnd = GetActiveWindow();
-
-		if (GetWindowRect(hwnd, out windowRect))
+		if (!Application.isEditor)
 		{
-			Debug.LogError("Couldn't get Window Rect");
-		}
+			margins = new Rectangle { Left = -1 };
+			hwnd = GetActiveWindow();
 
-		SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-		SetWindowPos(hwnd, HWND_TOPMOST, windowRect.Left, windowRect.Top, fWidth, fHeight, 32 | 64);
-		DwmExtendFrameIntoClientArea(hwnd, ref margins);
-#endif
+			UpdateWindowFlags();
+			DwmExtendFrameIntoClientArea(hwnd, ref margins);
+		}
 	}
 
-	void Update()
+	private void Update()
 	{
-		if (useSystemInput)
-		{
-			SystemInput.Process();
-		}
-
-		SetClickThrough();
+		UpdateWindowFlags();
 	}
 
-	//Returns true if the cursor is over a UI element or 2D physics object
-	bool FocusForInput()
+	private void UpdateWindowFlags()
 	{
-		EventSystem eventSystem = EventSystem.current;
-		if (eventSystem && eventSystem.IsPointerOverGameObject())
+		if (!Application.isEditor)
 		{
-			return true;
-		}
+			GetWindowRect(hwnd, out windowRect);
 
-		Vector2 pos = Camera.ScreenToWorldPoint(Input.mousePosition);
-		return Physics2D.OverlapPoint(pos, clickLayerMask);
-	}
-
-	void SetClickThrough()
-	{
-		var focusWindow = FocusForInput();
-
-		//Get window position
-		GetWindowRect(hwnd, out windowRect);
-
-#if !UNITY_EDITOR
-		if (focusWindow)
-		{
-			SetWindowLong (hwnd, -20, ~(((uint)524288) | ((uint)32)));
-			SetWindowPos(hwnd, HWND_TOPMOST, windowRect.Left, windowRect.Top, fWidth, fHeight, 32 | 64);
-		}
-		else
-		{
-			SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-			SetWindowLong (hwnd, -20, (uint)524288 | (uint)32);
+			var isFocused = isFocusedHandlers.Any(f => f(Input.mousePosition));
+			SetWindowLong(hwnd, GWL_STYLE, WindowStyle);
+			SetWindowLong (hwnd, GWL_EXSTYLE,  isFocused ? ~WindowExStyle : WindowExStyle);
 			SetLayeredWindowAttributes (hwnd, 0, 255, 2);
-			SetWindowPos(hwnd, HWND_TOPMOST, windowRect.Left, windowRect.Top, fWidth, fHeight, 32 | 64);
+			SetWindowPos(hwnd, HWND_TOPMOST, windowRect.Left, windowRect.Top, 
+				screenWidth, screenHeight, 32 | 64);
 		}
-#endif
 	}
 
-	public static void DragWindow()
+	public void DragWindow()
 	{
-#if !UNITY_EDITOR
-		if (Screen.fullScreenMode != FullScreenMode.Windowed)
+		if (!Application.isEditor)
 		{
-			return;
+			if (Screen.fullScreenMode != FullScreenMode.Windowed)
+			{
+				return;
+			}
+			
+			ReleaseCapture ();
+			SendMessage(hwnd, WM_SYSCOMMAND, WM_MOUSE_MOVE, 0);
+			Input.ResetInputAxes();
 		}
-		ReleaseCapture ();
-		SendMessage(Main.hwnd, WM_SYSCOMMAND, WM_MOUSE_MOVE, 0);
-		Input.ResetInputAxes();
-#endif		
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
@@ -177,4 +86,46 @@ public class TransparentWindow : MonoBehaviour
 		public int Right;
 		public int Bottom;
 	}
+	
+	/////////////////////
+	//Windows DLL stuff//
+	/////////////////////
+	
+	private const int GWL_STYLE = -16;
+	private const int GWL_EXSTYLE = -20;
+	private const uint WS_POPUP = 0x80000000;
+	private const uint WS_VISIBLE = 0x10000000;
+	private const uint WS_EX_LAYERED = 0x00080000;
+	private const uint WS_EX_TRANSPARENT = 0x00000020;
+	private const int HWND_TOPMOST = -1;
+	
+	private const uint WindowExStyle = WS_EX_LAYERED | WS_EX_TRANSPARENT;
+	private const uint WindowStyle = WS_POPUP | WS_VISIBLE;
+
+	private const int WM_SYSCOMMAND = 0x112;
+	private const int WM_MOUSE_MOVE = 0xF012;
+
+	[DllImport("user32.dll")]
+	private static extern IntPtr GetActiveWindow();
+	
+	[DllImport("user32.dll")]
+	private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+	[DllImport("user32.dll", EntryPoint = "SetLayeredWindowAttributes")]
+	private static extern int SetLayeredWindowAttributes(IntPtr hwnd, int crKey, byte bAlpha, int dwFlags);
+
+	[DllImport("user32.dll", EntryPoint = "GetWindowRect")]
+	private static extern bool GetWindowRect(IntPtr hwnd, out Rectangle rect);
+	
+	[DllImport("user32.dll")]
+	private static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+
+	[DllImportAttribute("user32.dll")]
+	private static extern bool ReleaseCapture();
+
+	[DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+	private static extern int SetWindowPos(IntPtr hwnd, int hwndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+
+	[DllImport("Dwmapi.dll")]
+	private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref Rectangle margins);
 }
